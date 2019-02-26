bb4a72df0a2709a6598dc8242ad0e363bcb5302a
s777610 <s777610@gmail.com>
2019-02-26 09:54:38 -0800

    Updates

---
+++ precache-manifest.d5fd1937138a85c06a869d04239e44ed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/progressive-web-app-exercise/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "bca895a003b2b758cac2",
+    "url": "/progressive-web-app-exercise/static/js/runtime~main.bca895a0.js"
+  },
+  {
+    "revision": "dcb642d5a4f4b7e5123c",
+    "url": "/progressive-web-app-exercise/static/js/main.dcb642d5.chunk.js"
+  },
+  {
+    "revision": "8267f31b26b02def4116",
+    "url": "/progressive-web-app-exercise/static/js/1.8267f31b.chunk.js"
+  },
+  {
+    "revision": "dcb642d5a4f4b7e5123c",
+    "url": "/progressive-web-app-exercise/static/css/main.03ccd4ca.chunk.css"
+  },
+  {
+    "revision": "8267f31b26b02def4116",
+    "url": "/progressive-web-app-exercise/static/css/1.4407ed0b.chunk.css"
+  },
+  {
+    "revision": "1fa8425089f9891d3556b999c43b0f0b",
+    "url": "/progressive-web-app-exercise/index.html"
+  }
+];
